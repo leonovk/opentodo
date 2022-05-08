f4830599b77775fe0b8959d9190778ce087a9d3d
Kirill <leonov835@yandex.ru>
2022-05-08 19:43:28 +0300

    get interactive menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+
+  def currently_at(page = '', numberlink)
+    case page
+
+    when 'Главная'
+      if numberlink == 1
+        "nav-link active"
+      else
+        "nav-link"
+      end
+
+    when 'Комнаты'
+      if numberlink == 2
+        "nav-link active"
+      else
+        "nav-link"
+      end
+
+    else
+      "nav-link"
+    end
+  end
+
   def full_title(page_title = "")
     base_title = "OpenToDo"
     if page_title.present?
