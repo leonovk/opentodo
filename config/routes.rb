e8e60243eb66f4629d89b742804f94443cc9d6eb
Kirill <leonov835@yandex.ru>
2022-05-27 23:43:27 +0300

    routes correction

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,17 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :rooms, only: [:edit, :update]
+  resources :rooms, only: [:edit, :update, :index, :create]
 
   resources :tasks, only: [:edit, :update]
   
-
-  get "/rooms", to: "rooms#index"
-  post "/rooms", to: "rooms#create"
   get "/rooms/:id", to: "rooms#show", as: 'current_room'
   post "/rooms/:id", to: "tasks#create"
   post "/rooms/:id/add/", to: "rooms#add"
   delete "/rooms/:id/delete/", to: "rooms#destroy"
-  ###
-
-  ###
   post "/rooms/tasks/status/:id", to: "tasks#status"
-  
   post "/", to: "tasks#create"
-  get "/admin", to: "admins#index"
-  
+  get "/admin", to: "admins#index" 
   root 'pages#index'
 
 end
