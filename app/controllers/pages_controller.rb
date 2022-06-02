128788ed47514c26ba5f7f6aebb1d2c59959a0ae
Kirill <leonov835@yandex.ru>
2022-06-02 20:52:10 +0300

    edit main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
     if user_signed_in?
-      @tasks = Task.where(user_id: current_user.id).order("id")
+      @tasks = Task.where(user_id: current_user.id, room_id: 0).order("id")
     end
   end
 end
