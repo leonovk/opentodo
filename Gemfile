c22ccff25555c889e63dafa2927029b55ed94195
Kirill <leonov835@yandex.ru>
2022-06-02 19:05:40 +0300

    change default locale

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "bootsnap", require: false
 gem 'bcrypt'
 gem 'sassc-rails'
 gem 'kaminari'
+gem 'rails-i18n'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,9 @@ Bundler.require(*Rails.groups)
 
 module Opentodo
   class Application < Rails::Application
-
     config.load_defaults 7.0
     config.time_zone = 'Europe/Moscow'
-
-
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :ru
   end
 end
