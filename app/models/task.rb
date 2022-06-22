d958056ddf963aafa42403b3f6c083bf3b8dd2c3
Kirill <leonov835@yandex.ru>
2022-06-22 23:51:23 +0300

    test

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :room ,optional: true
   validates :title, presence: true
+  after_create_commit { broadcast_append_to(:tasks, partial: "shared/task") }
 end
